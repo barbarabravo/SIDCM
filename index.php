5683b3038c776d97183193d4d08eecd3a70a4f86
Usuario <Usuario@ENVY>
2016-11-21 10:24:45 -0400

    CREACION DEL SISTEMA

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Sighm</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" />
+        <link href="css/font-awesome.min.css" rel="stylesheet" />
+    </head>
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" datatarget="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><i class="fa fa-user-md"></i> sighm</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Link</a></li>
+                </ul>
+            </div>
+        </nav>
+        <h1 class="text-center"><i class="fa fa-user-md"></i> consultas</h1>
+        <table class="table table-bordered">
+            <tr>
+                <td>celda1</td>
+                 <td>celda1</td>
+            </tr>
+            <tr>
+                <td>celda1</td>
+                 <td>celda1</td>
+            </tr>
+            <tr>
+                <td>celda1</td>
+                 <td>celda1</td>
+            </tr>
+            <tr>
+                <td>celda1</td>
+                 <td>celda1</td>
+            </tr>
+        </table>
+        <button class="btn btn-primary text-center"><i class="fa fa-sign-in"></i> salir</button>
+           
+    </body>
+</html>
